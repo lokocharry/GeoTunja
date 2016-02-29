490a57bb1c619b194a46a89587837e758a07ee34
Jorge Alberto Lozano Charry <jolberto_63@hotmail.com>
2016-02-29 11:24:13 -0500

    Función de vecino más cercano

--- consultas.sql
+++ consultas.sql
@@ -29,3 +29,30 @@ select * from pgr_astar(
 			HAVING b.id=nn(a.the_geom,0.00001,2,100))',
      4, 11, true, true);
 
+--Función de vecino más cercano
+create or replace function 
+  nn(nearTo                   geometry
+   , initialDistance          real
+   , distanceMultiplier       real 
+   , maxPower                 integer)
+ returns integer as $$
+declare 
+  sql     text;
+  result  integer;
+begin
+  sql := ' select id' 
+      || ' from rutas_tunja'
+      || ' where st_dwithin($1, ' 
+      ||   'the_geom' || ', $2 * ($3 ^ $4))'
+      || ' order by st_distance($1, ' || 'the_geom' || ')'
+      || ' limit 1';
+  for i in 0..maxPower loop
+     execute sql into result using nearTo              -- $1
+                                , initialDistance     -- $2
+                                , distanceMultiplier  -- $3
+                                , i;                  -- $4
+    if result is not null then return result; end if;
+  end loop;
+  return null;
+end
+$$ language 'plpgsql' stable;
